bb7d91843f638053b27f1789c39d73b2a901d27e
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-01-17 15:36:44 -0600

    Feat: test health controller to check status and response.body.

--- spec/requests/health_spec.rb
+++ spec/requests/health_spec.rb
@@ -3,8 +3,15 @@ require 'rails_helper'
 RSpec.describe 'Healths', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
+      # This will perform a GET request to the /health/index route
       get '/health/index'
-      expect(response).to have_http_status(:success)
+
+      # 'response' is a special object which contains HTTP response received after a request is sent
+      # response.body is the body of the HTTP response, which here contain a JSON string
+      expect(response.body).to eq('{"status":"online"}')
+
+      # we can also check the http status of the response
+      expect(response.status).to eq(200)
     end
   end
 end
